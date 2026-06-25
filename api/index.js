e732c4d8f81495b43f687af619861d7a1133d949
coderalitutorials <razamuneer670@gmail.com>
2026-06-25 15:50:43 -0700

    comment

--- api/index.js
+++ api/index.js
@@ -208,8 +208,8 @@ Phone: ${phone}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`${BRAND_NAME} server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`${BRAND_NAME} server running on port ${PORT}`);
+// });
 
 export default app;
